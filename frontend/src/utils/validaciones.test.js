8a9ce93fd9bf03867457d0d758c1c999181895c9
notbnn <ossali.ab@gmail.com>
2026-09-11 23:44:45 -0400

    test: caso culpable para probar el quality gate

--- frontend/src/utils/validaciones.test.js
+++ frontend/src/utils/validaciones.test.js
@@ -23,7 +23,7 @@ describe('esCorreoValido', () => {
 describe('contarTareasPendientes', () => {
   it('cuenta solo las tareas no completadas', () => {
     const tareas = [
-      { completada: true },
+      { completada: false },
       { completada: false },
       { completada: false },
     ]
